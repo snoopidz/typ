53852eaa71631ddb23e23b519d8bfec20752340e
beja benabid <snopidz@gmail.com>
2024-08-28 19:24:07 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  strings: ['Simple Design', 'Free To Use', 'Browser Safe', 'No Registration', 'SEO', 'Sky Blue'],
+  autoStart: true,
+  loop: true,
+});
+
+
